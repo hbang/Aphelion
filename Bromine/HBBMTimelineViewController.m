1839efa46fb154cc8c8bd64a8a213713ca0ff2b1
Adam D <kirbylover4000@gmail.com>
2013-07-24 01:11:15 +0930

    add STTwitter to xcodeproj

--- Bromine/HBBMTimelineViewController.m
+++ Bromine/HBBMTimelineViewController.m
@@ -17,7 +17,41 @@
 - (void)loadView {
 	[super loadView];
 	
-	self.title = L18N(@"Timeline");
+	self.title = @"Wat.";
+	
+	_tweets = [[NSMutableArray alloc] init];
+	_tweets[0] = [NSMutableDictionary dictionary];
+	_tweets[0][@"screen_name"] = @"sweg";
+}
+
+- (void)loadTweetsFromPath:(NSString *)path {
+	
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return _tweets.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	static NSString *CellIdentifier = @"CellIdentifier";
+	
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	
+	if (!cell) {
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier] autorelease];
+		
+		cell.selectionStyle = UITableViewCellSelectionStyleNone;
+	}
+	
+	cell.textLabel.text = [@"@" stringByAppendingString:_tweets[indexPath.row][@"screen_name"]];
+	
+	return cell;
 }
 
 @end
