594cf5f8c29ac0cf0d30c9a88283316de43f3292
Adam D <kirbylover4000@gmail.com>
2013-08-31 01:00:23 -0400

    avatar image view!

---
+++ Aphelion/HBAPAvatarImageView.m
@@ -0,0 +1,72 @@
+//
+//  HBAPAvatarImageView.m
+//  Aphelion
+//
+//  Created by Adam D on 30/08/13.
+//  Copyright (c) 2013 HASHBANG Productions. All rights reserved.
+//
+
+#import "HBAPAvatarImageView.h"
+#import "HBAPUser.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation HBAPAvatarImageView
+
++ (CGRect)frameForSize:(HBAPAvatarSize)size {
+	switch (size) {
+		case HBAPAvatarSizeSmall:
+			return CGRectMake(0, 9, 50.f, 50.f);
+			break;
+		
+		case HBAPAvatarSizeRegular:
+			return CGRectMake(0, 0, 64.f, 64.f);
+			break;
+		
+		case HBAPAvatarSizeOriginal:
+			return CGRectMake(0, 0, 500.f, 500.f);
+			break;
+	}
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];
+	
+	if (self) {
+		self.backgroundColor = [UIColor colorWithWhite:0.9f alpha:0.9f];
+	}
+	
+	return self;
+}
+
+- (instancetype)initWithScreenName:(NSString *)screenName size:(HBAPAvatarSize)size {
+	self = [self initWithFrame:[self.class frameForSize:size]];
+	
+	NSLog(@"initWithScreenName:size: not implemented");
+	
+	return self;
+}
+
+- (instancetype)initWithUser:(HBAPUser *)user size:(HBAPAvatarSize)size {
+	self = [self initWithFrame:[self.class frameForSize:size]];
+	
+	NSLog(@"initWithUser:size: not implemented");
+	
+	return self;
+}
+
+- (void)setFrame:(CGRect)frame {
+	[super setFrame:frame];
+	
+	self.layer.cornerRadius = frame.size.width / 2;
+}
+
+#pragma mark - Memory management
+
+- (void)dealloc {
+	[_url release];
+	[_screenName release];
+	
+	[super dealloc];
+}
+
+@end
