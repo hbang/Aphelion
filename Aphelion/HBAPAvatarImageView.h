79377847c3f199de965dc3c0250a5aafafccf612
Adam D <kirbylover4000@gmail.com>
2013-08-31 01:00:23 -0400

    avatar image view!

---
+++ Aphelion/HBAPAvatarImageView.h
@@ -0,0 +1,25 @@
+//
+//  HBAPAvatarImageView.h
+//  Aphelion
+//
+//  Created by Adam D on 30/08/13.
+//  Copyright (c) 2013 HASHBANG Productions. All rights reserved.
+//
+
+@class HBAPUser;
+
+typedef enum {
+	HBAPAvatarSizeSmall,
+	HBAPAvatarSizeRegular,
+	HBAPAvatarSizeOriginal,
+} HBAPAvatarSize;
+
+@interface HBAPAvatarImageView : UIImageView {
+	NSURL *_url;
+	NSString *_screenName;
+}
+
+- (instancetype)initWithScreenName:(NSString *)screenName size:(HBAPAvatarSize)size;
+- (instancetype)initWithUser:(HBAPUser *)user size:(HBAPAvatarSize)size;
+
+@end
