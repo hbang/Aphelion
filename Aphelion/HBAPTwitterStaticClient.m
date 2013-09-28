032239575542cfe4da0fd4cfc252fa45b53e5f89
Adam D <kirbylover4000@gmail.com>
2013-09-28 18:40:15 +0930

    support using api-provided entities for highlighting

--- Aphelion/HBAPTwitterStaticClient.m
+++ Aphelion/HBAPTwitterStaticClient.m
@@ -14,7 +14,7 @@
 	static HBAPTwitterStaticClient *sharedInstance = nil;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
-		sharedInstance = [[self.class alloc] initWithBaseURL:[NSURL URLWithString:kHBAPTwitterImageCDNRoot]];
+		sharedInstance = [[self.class alloc] init];
 	});
 	
 	return sharedInstance;
