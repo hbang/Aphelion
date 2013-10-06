a84eab7fe03143065c8222d9651a266e08d6cddc
Adam D <kirbylover4000@gmail.com>
2013-10-06 23:07:24 +1030

    yay cycript!

--- Aphelion/main.m
+++ Aphelion/main.m
@@ -9,8 +9,19 @@
 #import <UIKit/UIKit.h>
 #import "HBAPAppDelegate.h"
 
+#if DEBUG
+#import <Cycript/Cycript.h>
+#endif
+
 int main(int argc, char * argv[]) {
 	@autoreleasepool {
+#if DEBUG
+		CYListenServer(1337);
+		NSLog(@"cycript: listening on 1337");
+#else
+		NSLog(@"CRITICAL: CYCRIPT INCLUDED IN RELEASE BUILD");
+#endif
+		
 		return UIApplicationMain(argc, argv, nil, NSStringFromClass(HBAPAppDelegate.class));
 	}
 }
