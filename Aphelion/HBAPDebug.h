8e92a4b8726812ada2ee2dfab9523d61c23975bc
Adam D <kirbylover4000@gmail.com>
2013-10-06 23:53:51 +1030

    move debug stuff to HBAPDebug and log ip address

---
+++ Aphelion/HBAPDebug.h
@@ -0,0 +1,11 @@
+//
+//  HBAPDebug.h
+//  Aphelion
+//
+//  Created by Adam D on 6/10/13.
+//  Copyright (c) 2013 HASHBANG Productions. All rights reserved.
+//
+
+#if DEBUG
+void HBAPDebugStart();
+#endif
