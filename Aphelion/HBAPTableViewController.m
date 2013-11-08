095e3e0b516ba613cde100d792a253fc52cf53d6
Adam D <kirbylover4000@gmail.com>
2013-11-09 00:54:54 +1030

    move dynamic type reloading support to HBAPTableViewController

--- Aphelion/HBAPTableViewController.m
+++ Aphelion/HBAPTableViewController.m
@@ -15,8 +15,20 @@
 
 @implementation HBAPTableViewController
 
+- (void)loadView {
+	[super loadView];
+	
+	[[NSNotificationCenter defaultCenter] addObserver:self.tableView selector:@selector(reloadData) name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
 - (UIStatusBarStyle)preferredStatusBarStyle {
 	return [HBAPThemeManager sharedInstance].isDark ? UIStatusBarStyleLightContent : UIStatusBarStyleDefault;
+}
+
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver:self.tableView name:UIContentSizeCategoryDidChangeNotification object:nil];
+	
+	[super dealloc];
 }
 
 @end
